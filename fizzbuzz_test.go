6603d956512fd2a11e5678e91913b442b6308e30
Ivor Cheung <ivor.cheung@icloud.com>
2022-05-18 19:30:20 +0100

    adding table tests in

--- fizzbuzz_test.go
+++ fizzbuzz_test.go
@@ -2,13 +2,37 @@ package fizzbuzz
 
 import (
 	"testing"
-
-	"github.com/stretchr/testify/assert"
 )
 
-func testFizzBuzz(t *testing.T) {
-	got := 15
-	want := "Fizzbuzz"
+func TestIsFizzBuzz(t *testing.T) {
 
-	assert.Equal(t, fizzbuzz(got), want)
+	testCases := []struct {
+		input int
+		want  string
+	}{
+		{0, "FizzBuzz"},
+		{1, "1"},
+		{2, "2"},
+		{3, "Fizz"},
+		{4, "4"},
+		{5, "Buzz"},
+		{6, "Fizz"},
+		{7, "7"},
+		{8, "8"},
+		{9, "Fizz"},
+		{10, "Buzz"},
+		{11, "11"},
+		{12, "Fizz"},
+		{13, "13"},
+		{14, "14"},
+		{15, "FizzBuzz"},
+	}
+
+	for _, testCase := range testCases {
+		fizzBuzzResults := fizzbuzz(testCase.input)
+		if fizzBuzzResults != testCase.want {
+			t.Errorf("Got %s, expected %s", fizzBuzzResults, testCase.want)
+		}
+	}
+
 }
